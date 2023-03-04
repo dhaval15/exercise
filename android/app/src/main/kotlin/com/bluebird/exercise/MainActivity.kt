0a1ed7d6963314f67575eea1362a1fa1ffdc705e
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-03-04 15:25:40 -0500

    defined requirements

---
+++ android/app/src/main/kotlin/com/bluebird/exercise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bluebird.exercise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExerciseApp());
+}
+
+class ExerciseApp extends StatelessWidget {
+  const ExerciseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+

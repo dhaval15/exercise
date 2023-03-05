5a7cd496265847e9702952ec543ba94f7958b7d8
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-03-05 14:47:56 -0500

    added page indices buttons

--- lib/src/views/page_index_view.dart
+++ lib/src/views/page_index_view.dart
@@ -4,25 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
 class PageIndexController {
-
-  final  _controller = BehaviorSubject<int>();
-	final Function(int index)? onIndexChanged;
+  final _controller = BehaviorSubject<int>();
+  final Function(int index)? onIndexChanged;
 
   PageIndexController({
     int currentIndex = 0,
-		this.onIndexChanged,
+    this.onIndexChanged,
   }) {
-	 _controller.add(currentIndex);
-	}
+    _controller.add(currentIndex);
+  }
 
   Stream<int> get stream => _controller.stream;
 
-	int get currentIndex => _controller.value;
+  int get currentIndex => _controller.value;
 
-	void moveTo(int index) {
-		_controller.add(index);
-		onIndexChanged?.call(index);
-	}
+  void moveTo(int index) {
+    _controller.add(index);
+    onIndexChanged?.call(index);
+  }
 }
 
 class PageIndexView extends StatefulWidget {
@@ -42,37 +41,71 @@ class PageIndexView extends StatefulWidget {
 }
 
 class _PageIndexViewState extends State<PageIndexView> {
-  late int _currentIndex;
+  List<int> get _indices {
+    final list = <int>[];
+    int i = 1;
+    int currentIndex = widget.controller.currentIndex;
+    list.add(currentIndex);
 
-  @override
-  void initState() {
-    _currentIndex = widget.controller.currentIndex;
-    super.initState();
+    bool add(int value, {bool atFirst = false}) {
+      if (list.length < widget.numberOfIndicesToShow) {
+        if (atFirst) {
+          list.insert(0, value);
+        } else {
+          list.add(value);
+        }
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    while (true) {
+      if (currentIndex + i < widget.totalIndices && !(add(currentIndex + i))) {
+        break;
+      }
+      if (currentIndex - i >= 0 && !(add(currentIndex - i, atFirst: true))) {
+        break;
+      }
+      i++;
+    }
+    return list;
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Wrap(
       children: [
         TextButton(
-          onPressed: _currentIndex > 0
+          onPressed: widget.controller.currentIndex > 0
               ? () {
-                  setState(() {
-                    _currentIndex--;
-                  });
-                  widget.controller.moveTo(_currentIndex);
+                  widget.controller.moveTo(widget.controller.currentIndex - 1);
+                  setState(() {});
                 }
               : null,
           child: const Text('Prev'),
         ),
+        for (int index in _indices)
+          InkWell(
+            onTap: () {
+              widget.controller.moveTo(index);
+              setState(() {});
+            },
+            child: Container(
+              color: index == widget.controller.currentIndex
+                  ? Theme.of(context).colorScheme.onSurface.withOpacity(0.1)
+                  : Colors.transparent,
+              padding: const EdgeInsets.all(4),
+              child: Text(
+                '${index + 1}',
+              ),
+            ),
+          ),
         TextButton(
-          onPressed: _currentIndex < widget.totalIndices - 1
+          onPressed: widget.controller.currentIndex < widget.totalIndices - 1
               ? () {
-                  setState(() {
-                    _currentIndex++;
-                  });
-                  widget.controller.moveTo(_currentIndex);
+                  widget.controller.moveTo(widget.controller.currentIndex + 1);
+                  setState(() {});
                 }
               : null,
           child: const Text('Next'),
